c1a3d6472bd1318c019ceff10b2805023b4fade0
22473 <2247380761@qq.com>
2025-07-18 13:24:09 +0800

    后端修改

---
+++ crashAPI-backend/src/main/java/com/github/konstantyn111/crashapi/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.github.konstantyn111.crashapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CrashAPI 接口文档")
+                        .version("0.0.1")
+                        .description("用户认证与管理系统 API 文档")
+                        .contact(new Contact()
+                                .name("技术支持")
+                                .email("support@crashapi.com")))
+                .schemaRequirement("bearerAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+    }
+}
